e4728d3f96b06c4ee07ee69f35d2c7b360aaf84b
esdrasvitor2811 <167093885+esdrasvitor2811@users.noreply.github.com>
2024-04-15 11:05:58 -0300

    cartas

---
+++ cartas.py
@@ -0,0 +1,9 @@
+import random
+
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __str__(self):
+        return f"{self.valor} de {self.naipe}"
